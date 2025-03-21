7e3395481ba482a70c8c64251b96b0c4545e1c75
Jatin <heyjatinn@gmail.com>
2025-03-22 05:14:28 +0530

    feat: update Toaster component to use Sonner and improve theme handling; update package dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,8 +28,8 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Toaster />
         </ThemeProvider>
-        <Toaster />
       </body>
     </html>
   );
